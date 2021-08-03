295b67319619ab04c8b69cb3e77bd6af5e1bb23a
Qin <ctmakro@gmail.com>
2021-08-03 22:17:14 +0800

    .

--- vxq_tea_attendant_demo.py
+++ vxq_tea_attendant_demo.py
@@ -37,6 +37,9 @@ move_height = mcrcf['move_height']
 home_pos_up = home_pos.copy()
 home_pos_up[2] = move_height
 
+ik_move_speed = ims = 200
+joint_move_speed = jms = 400
+
 def tealoop():
     global button_texts, update_buttons
 
@@ -59,56 +62,45 @@ def tealoop():
 
             update_buttons()
 
-
+            should_break = 0
             if not master_only: # automatically add tea without master's command
                 if tst is not None and tst.has_solution():
 
-                    k = -1
                     for j in tag_combinations.keys():
-                        if j in tags and tag_combinations[j] not in tags:
-                            # only left in
-                            k = j
-                            break
-
-                        elif j in tags and tag_combinations[j] in tags: # both in
-                            if j in finish_adding_tea:
-                                # marked as finished in previous run
-                                del finish_adding_tea[j] # clear status
-
-                    if k==-1: # no target
-                        pass
-                    elif k not in finish_adding_tea:
-                        try_move_to_5(k)
-                        break
+                        if j in tags:
+                            if tag_combinations[j] not in tags:
+                                # only left in
+                                if j not in finish_adding_tea:
+                                    try_move_to_5(j)
+                                    should_break = 1
+                                    break
+                            else: # both in
+                                if j in finish_adding_tea:
+                                    # marked as finished in previous run
+                                    del finish_adding_tea[j] # clear status
 
             else: # only master can give command to add tea
                 if tst is not None and tst.has_solution():
 
-                    k = -1
-
                     # print('master_commanded.keys()', list(master_commanded.keys()))
                     for j in master_commanded.keys():
                     # for j in tag_combinations.keys():
-                        if j in tags and tag_combinations[j] not in tags:
-                            # only left in
-                            k = j
-                            break
+                        if j in tags:
+                            if tag_combinations[j] not in tags:
+                                # only left in
+                                try_move_to_5(j)
+                                should_break = 1
+                                if j in master_commanded:
+                                    del master_commanded[j]
+                                break
 
-                        elif j in tags and tag_combinations[j] in tags: # both in
-                            if j in finish_adding_tea:
-                                # marked as finished in previous run
-                                del finish_adding_tea[j] # clear status
+                            else: # both in
+                                if j in finish_adding_tea:
+                                    # marked as finished in previous run
+                                    del finish_adding_tea[j] # clear status
 
-                    if k==-1: # no target
-                        pass
-                    else:
-                        try_move_to_5(k)
-                        if k in master_commanded:
-                            del master_commanded[k]
-                        break
-
-
-
+            if should_break:
+                break
             time.sleep(0.3)
 
 def move_back():
@@ -116,10 +108,12 @@ def move_back():
     base_up = [0, 1, 600]
 
     # move to starting point
-    time.sleep(0.5)
+    # time.sleep(0.5)
     # v.g1_cartesian_joint(side_up, j1_first=False, speed=800)
-    v.g1_cartesian_joint(home_pos_up, j1_first=False, speed=800)
-    v.g1_cartesian_joint(home_pos, j1_first=False, speed=800)
+
+    v.g1_cartesian_ik(home_pos_up, speed=ims)
+    # v.g1_cartesian_joint(home_pos_up, j1_first=False, speed=800)
+    v.g1_cartesian_joint(home_pos, j1_first=False, speed=jms)
     time.sleep(0.5)
 
 def try_move_to_5(target=5):
@@ -149,7 +143,7 @@ def try_move_to_5(target=5):
                 tag5 = tags[left_tag]
                 # to the right of tag 5
                 t5c = Detection(tag5.marker_id, tag5.corners) # make copy
-                t5r = t5c.cxy + t5c.rxy * 3.2
+                t5r = t5c.rexy
 
                 screen = t5r
 
@@ -157,7 +151,7 @@ def try_move_to_5(target=5):
                 rc = at.transform(sc) # square to robot cartesian
 
                 default_height = -90
-                #default_height = mcrcf[0][2]
+                default_height = mcrcf[0][2]
                 #倒茶高度
 
                 # let's move! but from another thread...
@@ -165,43 +159,49 @@ def try_move_to_5(target=5):
                     try:
                         cancelled = lambda:v.cancel_movement
 
-                        v.g1_cartesian_joint(home_pos_up, j1_first=False, speed=800)
+                        v.g1_cartesian_joint(home_pos_up, j1_first=False, speed=jms)
                         if cancelled(): return
 
-                        v.g1_cartesian_joint(
+                        # v.g1_cartesian_joint(
                         # v.g1_cartesian_ik(
-                            list(rc)+[move_height],
-                            j1_first = True,
-                            speed=600,
+                        #     list(rc)+[move_height],
+                        #     # j1_first = True,
+                        #     speed=ims,
+                        #
+                        #     start=home_pos_up,
+                        # )
+                        v.g1_cartesian_ik(
+                            list(rc)+[default_height],
+                            # j1_first = True,
+                            speed=ims,
+
+                            start=home_pos_up,
                         )
                         if cancelled(): return
-                        v.g1_cartesian_joint(
-                        # v.g1_cartesian_ik(
-                            list(rc)+[default_height],
-                            j1_first = True,
-                            speed=600,
-                        )
+                        # v.g1_cartesian_joint(
+                        # # v.g1_cartesian_ik(
+                        #     list(rc)+[default_height],
+                        #     j1_first = True,
+                        #     speed=jms,
+                        # )
                         time.sleep(1)
 
                         if cancelled(): return
-                        # v.go_j(4, 1800) # 出水
+                        v.go_j(4, 1800) # 出水
                         if cancelled(): return
 
-                        time.sleep(1+3)
+                        time.sleep(3)
 
                         if cancelled(): return
                         v.go_j(4, 2048) # 停
-                        if cancelled(): return
 
-                        time.sleep(2)
-
-                        if cancelled(): return
+                        time.sleep(1)
 
                         v.g1_cartesian_joint(
                         # v.g1_cartesian_ik(
                             list(rc)+[move_height],
                             j1_first = True,
-                            speed=600,
+                            speed=jms,
                         )
 
                     except Exception as e:
@@ -220,10 +220,11 @@ def try_move_to_5(target=5):
                     # if 4 become visible, or
                     # tag 5 moved for more than size / 2
                     if right_tag in tags or (left_tag in tags and
-                        np.sum(np.square(tags[left_tag].cxy - t5c.cxy))
-                        > (t5c.size/2)**2):
+                        np.sqrt(np.sum(np.square(tags[left_tag].cxy - t5c.cxy
+                        )))) > (t5c.size*0.5):
 
                         # cancel_movement
+                        v.go_j(4, 2048) # 停
                         v.cancel_movement = True
                         th.join()
                         v.cancel_movement = False
